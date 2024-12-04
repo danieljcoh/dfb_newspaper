1223c51dc441342951091cc3bc6ce9ad9278de19
Daniel Cohen <danieljcoh@gmail.com>
2024-12-04 14:49:40 -0500

    added tests to make user vs superuser

--- accounts/admin.py
+++ accounts/admin.py
@@ -9,7 +9,7 @@ from .models import CustomUser
 
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
-    form = CustomUserCreationForm
+    form = CustomUserChangeForm
     list_display = [
         "email",
         "username",
